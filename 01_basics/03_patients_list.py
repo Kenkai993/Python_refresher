243645b88bc133c2fc86f6198607246f627a5c62
Stanišljević Miloš <milos.stanisljevic993@gmail.com>
2025-08-28 14:49:57 +0200

    Update 03_patients_list.py

--- 01_basics/03_patients_list.py
+++ 01_basics/03_patients_list.py
@@ -1,12 +1,13 @@
 dict_of_pat = [
-    {"Name": "Milos", "Godine": 32, "Dijagnoza": "Anksioznost"},
-    {"Name": "Stefan", "Godine": 40, "Dijagnoza": "Pneumonija"},
-    {"Name": "Boris", "Godine": 21, "Dijagnoza": "Tuberkuloza"},
-    {"Name": "Sara", "Godine": 19, "Dijagnoza": "HIV"},
-    {"Name": "Snjezana", "Godine": 46, "Dijagnoza": "Dijabetes"},
-
+        {"Name": "Milos", "Godine": 32, "Dijagnoza": "Anksioznost"},
+        {"Name": "Stefan", "Godine": 40, "Dijagnoza": "Pneumonija"},
+        {"Name": "Boris", "Godine": 21, "Dijagnoza": "Tuberkuloza"},
+        {"Name": "Sara", "Godine": 19, "Dijagnoza": "HIV"},
+        {"Name": "Snjezana", "Godine": 46, "Dijagnoza": "Dijabetes"}
+    ]
+    
 def remove_patient(pat_list, name):
     return [p for p in pat_list if p["Name"] != name]
-
+    
 dict_of_pat=remove_patient(dict_of_pat,"Stefan")
 print(dict_of_pat)
